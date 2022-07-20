68875e1bcc849a276b1d303d67ca61aa61c9fc60
chris <tigerman33@gmx.net>
2022-07-20 18:03:00 +0200

    Added entry point

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import Vector from './vector.js';
+import Segment from './segment.js';
+import Triangle from './triangle.js';
+import Polygon from './polygon.js';
+import Matrix from './matrix.js';
+
+export {
+	Vector,
+	Segment,
+	Triangle,
+	Polygon,
+	Matrix
+}
