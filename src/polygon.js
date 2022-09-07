d2aaabc89d497f5c63e01d750fc7decc9cb2cfe1
chris <tigerman33@gmx.net>
2022-09-07 18:31:36 +0200

    Based boolean operations on polygons on external library.
    The polygon is first projected into 2D, then run through the external library, then the result is expanded back into 3D.

--- src/polygon.js
+++ src/polygon.js
@@ -1,5 +1,179 @@
 import Vector from './vector.js';
 import Constants from './constants.js';
+import pc from 'polygon-clipping';
+import winding from './util/winding.js';
+import sign from './util/sign.js';
+
+function oper(poly1, poly2, mode) {
+	// Make sure poly1 and poly2 are co-planar
+	const normal = poly1.normal;
+
+	const axis = [...'xyz'].reduce((prev, curr) => normal[curr] >= normal[prev] ? curr : prev);
+	const [ dim1, dim2 ] = [...'xyz'].filter(dim => dim !== axis);
+	const d = -Vector.dot(normal, poly1[0]);
+
+	// Check that polygons are co-planar
+	// They are co-planar if their normals are parallel, and if an arbitrary point from poly2 is also on the poly1 plane
+	if (!normal.equals(poly2.normal.scale(normal[axis] / poly2.normal[axis])) || sign(Vector.dot(normal, poly1[0]) - Vector.dot(normal, poly2[0])) !== 0) {
+		throw new Error(`Cannot perform ${mode} on polygons that are not co-planar`);
+	}
+
+	let result = pc[mode]([poly1.map(vertex => [ vertex[dim1], vertex[dim2] ])], [poly2.map(vertex => [ vertex[dim1], vertex[dim2] ])]);
+	// Make each polygon a collection of coordinates, instead of a collection of linear rings
+	result = result.map(poly => poly.flat());
+
+	if (result.length > 2 && result.filter(poly => winding(poly) === +1).length > 1)
+		throw new Error(`Expected a polygon with at most one hole, but got ${result.length - 1} holes`);
+	
+	result.map(poly => {
+		// If the first and the last point are equal, remove the last point
+		if (poly[0].every((coord, index) => Math.abs(coord - poly.at(-1)[index]) < Constants.EPSILON))
+			poly.pop();
+		return poly;
+	});
+
+	if (result.length > 1 && winding(result[1]) === +1) {
+		const exterior = result[0];
+		const hole = result[1];
+
+		/*		
+			Unfortunately, our data structure is not designed for polygons with holes.
+			So instead, we will bisect the result polygon in such a way that is guaranteed to create
+			crossing intersections with the hole.
+			
+			Specifically, we will calculate the y-coordinate that is halfway between the hole's
+			lowest and highest point. (Actually, any value strictly greater than the lowest and strictly less
+			that the highest would work.) 
+
+			Our bisection line is a horizontal line with that y-coordinate. This is guaranteed to have at least
+			two crossing intersections with the hole. Because the hole is contained in the 
+			polygon, it follows that it has at least two crossing intersections with the
+			polygon as well.
+
+			We now find the left-most intersection C1 and the right-most intersection C2 between the 
+			hole and the bisection line. We then find S1 as the closest intersection to the left of C1 between
+			the polygon and the bisection line, and likewise we find S2 as the closest intersection to the right
+			of C2. (Note that these are guaranteed to exist since the hole is contained entirely within the
+			polygon.)
+			
+			We now construct the result polygons: One is formed by the edges between S1 and S2 travelling along
+			the top portion of the polygon, connecting S2 to C2, travelling along the top portion between C2 and
+			C1, and connecting C1 to S1.
+			Similarly, the other is formed by travelling along the bottom portion between S2 and S1, connecting
+			S1 and C1, travelling along the bottom portion of the hole to C2, and connecting C2 and S2.
+		*/
+	
+		// Find the bisection line
+		let y = (Math.min(...hole.map(v => v[1])) + Math.max(...hole.map(v => v[1]))) / 2;
+
+		// The left-most and right-most intersections of the hole with the bisection line
+		let C1;
+		let C2;
+		// The indices of the starting points of the edges that C1 and C2 are on
+		let indexC1;
+		let indexC2;
+		for (let i = 0; i < hole.length; i++) {
+			let Q1 = hole[i];
+			let Q2 = hole[(i + 1) % hole.length];			
+			if (Q1[1] <= y && Q2[1] > y) {
+				// The GeoJSON spec guarantees hole to be wound clockwise,
+				// so if Q1.y <= y < Q2.y, that is guaruanteed to be a candidate for the left-most intersection C1.
+				let alpha = (y - Q1[1]) / (Q2[1] - Q1[1]);
+				let x = Q1[0] + alpha * (Q2[0] - Q1[0]);
+
+				if (x < C1[0] || !C1)  {
+					C1 = [ x, y ];
+					indexC1 = i;
+				}
+			} else if (Q1[1] >= y && Q2[1] < y) {
+				// For the same reasons as above, this can only be a candidate for the right-most intersection C2. 
+				let alpha = (y - Q1[1]) / (Q2[1] - Q1[1]);
+				let x = Q1[0] + alpha * (Q2[0] - Q1[0]);
+
+				if (x > C2[0] || !C2) {
+					C2 = [ x, y ];
+					indexC2 = i;
+				}
+			}
+		}
+		
+		// Find the intersection S1 that is closest to the left of C1, and S2 that is closest to the right of C2.
+		
+		let S1;
+		let S2;
+		let indexS1;
+		let indexS2;
+		for (let i = 0; i < exterior.length; i++) {
+			let P1 = exterior[i];
+			let P2 = exterior[(i + 1) % exterior.length];
+			if (P1[1] <= y && P2[1] > y) {
+				// Because the exterior ring is wound counter-clockwise, this can only be a candidate for the
+				// right intersection S2.
+				let alpha = (y - P1[1]) / (P2[1] - P1[1]);
+				let x = P1[0] + alpha * (P2[0] - P1[0]);
+				if (!S2 || Math.abs(x - C2[0]) < Math.abs(S2[0] - C2[0])) {
+					S2 = [ x, y ];
+					indexS2 = i;
+				} 
+			} else if (P1[1] >= y && P2[1] < y) {
+				// For the same reasons, this can only be a candidate for the left intersection S1.
+				let alpha = (y - P1[1]) / (P2[1] - P1[1]);
+				let x = P1[0] + alpha * (P2[0] - P1[0]);
+				if (!S1 || Math.abs(x - C1[0]) < Math.abs(S1[0] - C1[0])) {
+					S1 = [ x, y ];
+					indexS1 = i;
+				}
+			}
+		}
+
+		// Make sure that S1 comes after S2 on the exterior, and C2 comes after C1 on the hole
+		if (indexS1 < indexS2)
+			indexS1 += exterior.length;
+		if (indexC2 < indexC1)
+			indexC2 += hole.length;
+
+		let top = [];
+		let bottom = [];
+
+		// Trace the exterior and add points to the correct result polygon.
+		// We will start at the edge AFTER S2 and move around the polygon,
+		// adding points to the top result polygon until we reach S1, and 
+		// to the pottom polygon after.
+		for (let i = indexS2 + 1; i < indexS2 + 1 + exterior.length; i++) {
+			if (i <= indexS1) 
+				top.push(exterior[i % exterior.length]);
+			else
+				bottom.push(exterior[i % exterior.length]);
+		}
+		top.push(S1);
+		top.push(C1);
+		bottom.push(S2);
+		bottom.push(C2);
+		// Trace the hole and add points to the correct result polygon
+		for (let i = indexC1 + 1; i < indexC1 + 1 + hole.length; i++) {
+			if (i <= indexC2)
+				top.push(hole[i % hole.length]);
+			else
+				bottom.push(hole[i % hole.length]);
+		}
+		top.push(C2);
+		top.push(S2);
+		bottom.push(C1);
+		bottom.push(S1);
+
+		result = [ top, bottom ];
+	}
+
+	result = result.map(poly => 
+		poly.map(vertex => new Vector({
+			[dim1]: vertex[0],
+			[dim2]: vertex[1],
+			[axis]: -(normal[dim1] * vertex[0] + normal[dim2] * vertex[1] + d) / normal[axis]
+		})
+	));
+
+	return result;
+}
 
 export default class Polygon extends Array {
 	constructor() {
@@ -101,6 +275,10 @@ export default class Polygon extends Array {
 			}
 		}
 		return true;
+	}
+
+	intersect(other) {
+		return oper(this, other, 'intersection');
 	}
 
 	isEmpty() { 
--- test/polygon.spec.js
+++ test/polygon.spec.js
@@ -1,7 +1,37 @@
 import Vector from '../src/vector.js';
 import Polygon from '../src/polygon.js';
+import pc from 'polygon-clipping';
+import { readFileSync } from 'fs';
 
 const EPSILON = 1.0e-8;
+
+function reorder(actual, expected) {
+	let index = actual.findIndex(vertex => expected[0].equals(vertex));
+	if (index > -1)
+		actual = actual.slice(index).concat(actual.slice(0, index));
+	return actual;
+}
+
+function expectPolyEqual(actual, expected) {
+	// Reorder actual polygon because it might be in reverse order/starting at a different vertex than the expected polygon
+	actual = reorder(actual, expected);
+	// Now they should be deeply equal. Unfortunately we can't use deep equality directly here though,
+	// because we need to account for floating-point imprecision.
+	// So we need to compare every vertex by hand here.	
+	expected.forEach((vertex, index) => {
+		expect(actual[index].x).to.be.approximately(vertex.x, EPSILON);
+		expect(actual[index].y).to.be.approximately(vertex.y, EPSILON);
+	});
+}
+
+function expectMultiPolyEqual(actual, expected) {
+	expect(actual).to.be.an('array').with.lengthOf(expected.length);
+	expected.forEach(expectedComponent => {
+		let actualComponent = actual.find(component => component.every(actualVertex => expectedComponent.some(expectedVertex => Math.abs(actualVertex.x - expectedVertex.x) < EPSILON && Math.abs(actualVertex.y - expectedVertex.y) < EPSILON)));
+		expect(actualComponent).to.exist;
+		expectPolyEqual(actualComponent, expectedComponent);
+	});
+}
 
 describe('Polygon', function() {
 	describe('constructor', function() {
@@ -19,7 +49,7 @@ describe('Polygon', function() {
 		it('should construct a polygon from an array of points', function() {
 			expect(new Polygon(vertices)).to.deep.equal(vertices);
 		});
-	})
+	});
 
 	describe('.normal', function() {
 		it('should compute the normal for a polygon with three or more vertices', function() {
@@ -103,7 +133,7 @@ describe('Polygon', function() {
 
 		it('should equal another polygon with the same vertices, regardless of start point', function() {
 			let other = new Polygon(...polygon);
-			expect(polygon.equals(other));
+			expect(polygon.equals(other)).to.be.true;
 
 			const idx = 3;
 			other = new Polygon(...other.slice(idx).concat(other.slice(0, idx)));
@@ -130,10 +160,103 @@ describe('Polygon', function() {
 		});
 
 		it('should return true for convex polygons, false for concave ones', function() {
-			
 			expect(polygon.isConvex()).to.be.true;
 			polygon[4].x = 2;
 			expect(polygon.isConvex()).to.be.false;
 		});
 	});
+
+	describe.only('.intersect', function() {
+		it('should throw when attempting to intersect polygons that are not co-planar', function() {
+			const polygon = new Polygon(
+				new Vector(1, 1, 1),
+				new Vector(3, 1, 1),
+				new Vector(2, 5, 1),
+				new Vector(-3, 5, 1),
+				new Vector(-2, 2, 1),
+				new Vector(1, 3, 1)
+			);
+			let clip = new Polygon(polygon.map(vertex => vertex.add(polygon.normal)));
+			expect(polygon.intersect.bind(polygon, clip), 'parallel but not co-planar').to.throw();
+			
+			clip = new Polygon(({ x, y} , index) => new Vector(x, y, index));
+			expect(polygon.intersect.bind(polygon, clip), 'not parallel').to.throw();
+		});
+
+		it('should intersect co-planar overlapping polygons', function() {
+			const polygonVertices = JSON.parse(readFileSync('test/fixtures/intersect_subject.json'));
+			const clipVertices = JSON.parse(readFileSync('test/fixtures/intersect_clip.json'));
+			const expectedVertices = JSON.parse(readFileSync('test/fixtures/intersect_expected.json'));
+
+			// We will run this test with the polygons lying on two different planes.
+			// The first time around, they will be on the x-y-plane, to better show faulty intersection calculation
+			// The second time, they will be on an arbitrary (skew) plane, to better show faulty re-extrapolation
+			// to the original plane.
+			[
+				// z-coordinate for given (x,y) for the x-y-plane
+				() => 0,
+				// z-coordinate for given (x,y) for the (arbitrary) plane -2x + 3y + 4z -5 = 0
+				({ x, y }) => (2*x - 3*y + 5) / 4,
+			].forEach(z => {
+				const polygon = new Polygon(polygonVertices.map(vertex => new Vector({ ...vertex, z: z(vertex) })));
+				const clip = new Polygon(clipVertices.map(vertex => new Vector({ ...vertex, z: z(vertex) })));
+
+				const expected = expectedVertices
+					.map(poly =>
+					poly.map(vertex => new Vector({ ...vertex, z: z(vertex) })));
+
+				let result = polygon.intersect(clip);
+				expect(result).to.be.an('array').with.lengthOf(2);
+				expectMultiPolyEqual(result, expected);
+			});
+		});
+	});
+
+	it('.difference', function() {
+			// Subject looks like a letter "c"
+			const subject =[
+				[1, 1],
+				[3, 1],
+				[2, 5],
+				[-3, 5],
+				[-2, 2]
+			];
+			const clip = [ 
+				[-1, 2],
+				[2, 2],
+				[2, 4],
+				[-1, 4],
+				[-2, 3]
+			];
+
+			let result = pc.difference(
+				[ subject ],
+				[ clip ])
+			expect(result).to.be.an('array').with.lengthOf(1);
+			result = reorder(result[0], expected);			
+			expect(result).to.deep.equal(expected);
+			console.log(JSON.stringify(result));		
+	});
+
+	it('.union', function() {
+					// Subject looks like a letter "c"
+			const subject =[
+				{ x: 0, y: 0 },
+				{ x: 0, y: 1 },
+				{ x: -2, y: 1 },
+				{ x: -2, y: 2 },
+				{ x: 0, y: 2 },
+				{ x: 0, y: 3 },
+				{ x: -3, y: 3 },
+				{ x: -3, y: 0 }
+			];
+			// Clip is the y-axis mirror image of subject
+			const clip = subject.map(vertex => ({ x: Math.abs(vertex.x), y: vertex.y }));
+
+			let result = pc.union(
+				[ subject.map(vertex => [ vertex.x, vertex.y ]) ],
+				[ clip.map(vertex => [ vertex.x, vertex.y ]) ])
+			console.log(result[0]);		
+
+	})
 });
